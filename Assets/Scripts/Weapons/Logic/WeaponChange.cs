ee62f8f4f68368c74dd03523bf3bdf911f28dc66
Marco199609 <marco19-96@hotmail.com>
2023-01-20 09:14:20 -0300

    Added weapon scroll change

--- Assets/Scripts/Weapons/Logic/WeaponChange.cs
+++ Assets/Scripts/Weapons/Logic/WeaponChange.cs
@@ -6,6 +6,10 @@ public class WeaponChange : MonoBehaviour
 {
     public WeaponData currentWeaponData { get; private set; }
 
+    private int _currentWeaponIndex = 0;
+
+    private bool _isSwitchingWeapon;
+
     public void ChangeWeapon(GameObject[] weapons, WeaponInput weaponInput)
     {
         if(currentWeaponData == null)
@@ -13,7 +17,57 @@ public class WeaponChange : MonoBehaviour
             SetDefaultNoWeapon(weapons);
         }
 
-        if(weaponInput.weaponIndexInput >= 0 && (weaponInput.weaponIndexInput) < weapons.Length) //Checks if weapon index is in array bounds
+        ChangeWeaponWithNumberKeys(weapons, weaponInput);
+        ChangeWeaponWithMouseScroll(weapons, weaponInput);
+    }
+
+    private void SetDefaultNoWeapon(GameObject[] weapons)
+    {
+        weapons[0].SetActive(true);
+        currentWeaponData = weapons[0].GetComponent<WeaponData>();
+    }
+
+    private void ChangeWeaponWithMouseScroll(GameObject[] weapons, WeaponInput weaponInput)
+    {
+        if (weaponInput.mouseScrollInput > 0 && _isSwitchingWeapon == false && !ObjectManager.Instance.PlayerInput.FlashLightInput)
+        {
+            _currentWeaponIndex++;
+            _isSwitchingWeapon = true;
+        }
+        else if (weaponInput.mouseScrollInput < 0 && _isSwitchingWeapon == false && !ObjectManager.Instance.PlayerInput.FlashLightInput)
+        {
+            _currentWeaponIndex--;
+            _isSwitchingWeapon = true;
+        }
+        
+        if (_currentWeaponIndex < 0) _currentWeaponIndex = weapons.Length;
+        else if (_currentWeaponIndex > weapons.Length) _currentWeaponIndex = 0;
+
+        //Change weapon
+        if(_isSwitchingWeapon)
+        {
+            for (int i = 0; i < weapons.Length; i++)
+            {
+                if (weapons[i] != null)
+                {
+                    //Enables weapon selected and changes current weapon data for weapon controller
+                    if (i == _currentWeaponIndex)
+                    {
+                        weapons[i].SetActive(true);
+                        currentWeaponData = weapons[i].GetComponent<WeaponData>();
+                    }
+                    else weapons[i].SetActive(false);
+                }
+                else if (weapons[i - 1] == null) SetDefaultNoWeapon(weapons); //Sets weapon 0 only if no weapon on both slots
+            }
+        }
+
+        if (weaponInput.mouseScrollInput == 0) _isSwitchingWeapon = false;
+    }
+
+    private void ChangeWeaponWithNumberKeys(GameObject[] weapons, WeaponInput weaponInput)
+    {
+        if (weaponInput.weaponIndexInput >= 0 && (weaponInput.weaponIndexInput) < weapons.Length) //Checks if weapon index is in array bounds
         {
             for (int i = 0; i < weapons.Length; i++)
             {
@@ -25,21 +79,11 @@ public class WeaponChange : MonoBehaviour
                         weapons[i].SetActive(true);
                         currentWeaponData = weapons[i].GetComponent<WeaponData>();
                     }
-                    else
-                        weapons[i].SetActive(false);
+                    else weapons[i].SetActive(false);
                 }
-                else if (weapons[i-1] == null) //Sets weapon 0 only if no weapon on both slots
-                {
-                    SetDefaultNoWeapon(weapons);
-                }
+                else if (weapons[i - 1] == null) SetDefaultNoWeapon(weapons); //Sets weapon 0 only if no weapon on both slots
 
             }
         }
-    }
-
-    private void SetDefaultNoWeapon(GameObject[] weapons)
-    {
-        weapons[0].SetActive(true);
-        currentWeaponData = weapons[0].GetComponent<WeaponData>();
     }
 }
