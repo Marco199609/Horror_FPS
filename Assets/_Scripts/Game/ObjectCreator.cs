f578e4c98e03210df487a5db4ccc1ba87ae23c6b
Marco199609 <marco19-96@hotmail.com>
2023-01-22 02:47:17 -0300

    Made basic menu

--- Assets/_Scripts/Game/ObjectCreator.cs
+++ Assets/_Scripts/Game/ObjectCreator.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectCreator
+public class ObjectCreator : MonoBehaviour 
 {
-    private static DontDestroyOnLoad _persistentSystemsParent;
+    private static Transform _persistentSystemsParent;
     private static List<GameObject> _objects;
 
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-    public static void Execute()
+    //[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    private void Awake()
     {
-        _persistentSystemsParent = Object.FindObjectOfType<DontDestroyOnLoad>();
+        Execute();
+    }
+
+
+    //public static void Execute()
+    public void Execute()
+    {
+        _persistentSystemsParent = this.transform;
         _objects = new List<GameObject>();
 
         CreateObject<ObjectManager>();
@@ -30,7 +37,8 @@ public class ObjectCreator
         }
     }
 
-    public static void CreateObject<T>() where T : MonoBehaviour
+    //public static void CreateObject<T>() where T : MonoBehaviour
+    public void CreateObject<T>() where T : MonoBehaviour
     {
         if (Object.FindObjectOfType<T>() != null) return;
 
