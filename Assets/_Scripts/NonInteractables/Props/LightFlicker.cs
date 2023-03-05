76628baa5b9532101271fc94a19f99ad79bf9afb
Marco199609 <marco19-96@hotmail.com>
2023-03-05 17:03:06 -0300

    Light flicker and exterior lighting

---
+++ Assets/_Scripts/NonInteractables/Props/LightFlicker.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    [SerializeField] private float _timer = 1f;
+    [SerializeField] private Material _emisiveMaterial, _opaqueMaterial;
+    [SerializeField] private Renderer _lamp;
+    private bool _lightOn;
+    private Light _light;
+
+    private void Awake()
+    {
+        _light = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        _timer -= Time.deltaTime;
+
+        if(_timer < 0 )
+        {
+            if (_lightOn)
+            {
+                _light.enabled = false;
+                _lamp.material = _opaqueMaterial;
+                _timer = UnityEngine.Random.Range(0.5f, 1.5f);
+                _lightOn = false;
+            }
+            else
+            {
+                _light.enabled = true;
+                _lamp.material = _emisiveMaterial;
+                _timer = UnityEngine.Random.Range(0.5f, 1.5f);
+                _lightOn = true;
+            }
+        }
+    }
+}
--- Assets/_Scripts/Player/Logic/PlayerRotate.cs
+++ Assets/_Scripts/Player/Logic/PlayerRotate.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 
 public class PlayerRotate : MonoBehaviour, IPlayerRotate
 {
+
+    private float _rotationX = 0F;
+    private float _rotationY = 0F;
     private float _minimumY = -60F;
     private float _maximumY = 60F;
-    private float _rotationY = 0F;
 
     private PlayerData _playerData;
     private Transform _camHolder;
+
+    private void Awake()
+    {
+        _playerData = FindObjectOfType<PlayerData>();
+
+        _rotationX = _playerData.gameObject.transform.localEulerAngles.y;
+        _rotationY = _playerData.camHolder.transform.localEulerAngles.x;
+    }
 
     public void RotatePlayer(GameObject player, IPlayerInput playerInput)
     {
         if (_playerData == null) _playerData = player.GetComponent<PlayerData>();
         if (_camHolder == null) _camHolder = _playerData.camHolder;
 
-        float rotationX = player.transform.localEulerAngles.y + playerInput.mouseMovementInput.x * _playerData.mouseSensitivityX * Time.deltaTime;
+        _rotationX = player.transform.localEulerAngles.y + playerInput.mouseMovementInput.x * _playerData.mouseSensitivityX * Time.deltaTime;
 
         _rotationY += playerInput.mouseMovementInput.y * _playerData.mouseSensitivityY * Time.deltaTime;
         _rotationY = Mathf.Clamp(_rotationY, _minimumY, _maximumY);
 
-        player.transform.eulerAngles = Vector3.Lerp(player.transform.eulerAngles, new Vector3(0, rotationX, 0), 60f * Time.deltaTime);
+        player.transform.eulerAngles = Vector3.Lerp(player.transform.eulerAngles, new Vector3(0, _rotationX, 0), 60f * Time.deltaTime);
         //_camHolder.localEulerAngles = Vector3.Lerp(_camHolder.localEulerAngles, new Vector3(-_rotationY, 0, 0), 200f * Time.deltaTime);
 
         /*Previous implementation
