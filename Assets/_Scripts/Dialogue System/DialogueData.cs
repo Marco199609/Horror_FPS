a19de4e24e11edaa8149d6f957619e43402ed152
Marco199609 <marco19-96@hotmail.com>
2023-08-07 02:40:34 -0400

    Added json template

---
+++ Assets/_Scripts/Dialogue System/DialogueData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueData : MonoBehaviour
+{
+    public AudioClip[] DialogueClips;
+    public string[] DialogueSubtitles;
+}
