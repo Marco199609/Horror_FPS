f85a19813668f19fbc49037b477f6aeb9c7014f0
Marco199609 <marco19-96@hotmail.com>
2023-08-21 03:05:11 -0400

    Added dialogue to various props

---
+++ Assets/_Scripts/Interactables/Props/Prop_DialogueTrigger.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prop_DialogueTrigger : MonoBehaviour, IInteractable
+{
+    [SerializeField] private int _id;
+    [SerializeField] private bool _nonInspectable, _inspectableOnly;
+
+    [Header("Use only if inspectable")]
+    [SerializeField] private bool _rotateX = true;
+    [SerializeField] private bool _rotateY = true;
+    [SerializeField] private bool _rotateZ = true;
+
+    [SerializeField] private Trigger_DialogueSystem[] _dialogueTriggers;
+
+    private Collider _collider;
+    private float _reactivateColliderTimer = 1f; //Prevents dialogue trigger overlap
+    public void AssignInStateLoader()
+    {
+        SceneStateLoader.Instance.objects.Add(_id, gameObject);
+    }
+
+    public void Interact(PlayerController playerController)
+    {
+        StartCoroutine(DisableColliderTemporarily(_reactivateColliderTimer));
+
+        bool[] rotateXYZ = new bool[] { _rotateX, _rotateY, _rotateZ };
+        if(!_nonInspectable) playerController.PlayerInspect.Inspect(transform, rotateXYZ);
+    }
+
+    IEnumerator DisableColliderTemporarily(float timer)
+    {
+        if (_collider == null) _collider = GetComponent<Collider>();
+        _collider.enabled = false;
+
+        yield return new WaitForSeconds(timer);
+
+        _collider.enabled = true;
+    }
+
+    public bool[] InteractableType()
+    {
+        bool[] interactableType = new bool[] { _nonInspectable, _inspectableOnly };
+        return interactableType;
+    }
+
+    public bool[] RotateXYZ()
+    {
+        bool[] rotateXYZ = new bool[] { _rotateX, _rotateY, _rotateZ };
+        return rotateXYZ;
+    }
+
+    public void TriggerActions()
+    {
+        int dialogueIndex = Random.Range(0, _dialogueTriggers.Length);
+        _dialogueTriggers[dialogueIndex].TriggerAction(0);
+    }
+}
